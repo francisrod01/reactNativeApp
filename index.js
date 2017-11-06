06fedb19ae2b62cf6df8410f379db5964d35ac1a
Francis Rodrigues <dev@tosystems.net>
2017-11-06 03:26:27 -0200

    Implemented first Greeting component

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { AppRegistry } from 'react-native';
 // import App from './src/App';
 // import App from './src/HelloWorldApp'
-import App from './src/Bananas'
+// import App from './src/Bananas'
+import App from './src/Greeting'
 
 AppRegistry.registerComponent('reactNativeApp', () => App);
