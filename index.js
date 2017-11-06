899f71b7bafa3034c3855b6f08361f534940baf0
Francis Rodrigues <dev@tosystems.net>
2017-11-06 14:26:49 -0200

    Implemented FlexDimensionsBasics component

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { AppRegistry } from 'react-native';
 // import App from './src/Greeting'
 // import App from './src/Blink'
 // import App from './src/LotsOfStyles'
-import App from './src/FixedDimensionsBasics'
+// import App from './src/FixedDimensionsBasics'
+import App from './src/FlexDimensionsBasics'
 
 AppRegistry.registerComponent('reactNativeApp', () => App);
