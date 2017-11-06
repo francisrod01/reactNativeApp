cdaac72af5e7df9013e04d60d3bac128452fa787
Francis Rodrigues <dev@tosystems.net>
2017-11-06 17:26:37 -0200

    Improved react-native source structure

--- src/components/HelloWorldApp.js
+++ src/components/HelloWorldApp.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
-import {Text} from 'react-native';
+import React, {Component} from 'react'
+import {Text} from 'react-native'
 
 export default class HelloWorldApp extends Component {
     render() {
         return (
             <Text>Hello world!</Text>
-        );
+        )
     }
 }
