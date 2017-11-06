8246ffcc48e484d8cf7de2a161577485caf8c2dd
Francis Rodrigues <dev@tosystems.net>
2017-11-06 14:14:05 -0200

    Implemented lots of styles as component

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { AppRegistry } from 'react-native';
 // import App from './src/HelloWorldApp'
 // import App from './src/Bananas'
 // import App from './src/Greeting'
-import App from './src/Blink'
+// import App from './src/Blink'
+import App from './src/LotsOfStyles'
 
 AppRegistry.registerComponent('reactNativeApp', () => App);
