f6fc011a2166134f6887b96053e0cbacbe60acad
Francis Rodrigues <dev@tosystems.net>
2017-11-06 15:33:45 -0200

    Implemented FlexDimensionsAlignCenter layout

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import { AppRegistry } from 'react-native';
-// import App from './src/App';
+import {AppRegistry} from 'react-native'
+// import App from './src/App'
 // import App from './src/HelloWorldApp'
 // import App from './src/Bananas'
 // import App from './src/Greeting'
 // import App from './src/Blink'
 // import App from './src/LotsOfStyles'
-// import App from './src/FixedDimensionsBasics'
+// import App from './src/layout/FixedDimensionsBasics'
 // import App from './src/layout/FlexDimensionsBasics'
 // import App from './src/layout/FlexDimensionsHorizontally'
-import App from './src/layout/FlexDimensionsJustify'
+// import App from './src/layout/FlexDimensionsJustify'
+import App from './src/layout/FlexDimensionsAlignCenter'
 
-AppRegistry.registerComponent('reactNativeApp', () => App);
+AppRegistry.registerComponent('reactNativeApp', () => App)
