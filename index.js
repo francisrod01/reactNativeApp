8cbda2571031d9550eceeb7c6f245995ac5c332b
Francis Rodrigues <dev@tosystems.net>
2017-11-06 03:23:03 -0200

    Implemented first Image component

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 // import App from './src/App';
-import App from './src/HelloWorldApp';
+// import App from './src/HelloWorldApp'
+import App from './src/Bananas'
 
 AppRegistry.registerComponent('reactNativeApp', () => App);
