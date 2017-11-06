68b469043f0521a5c0b10e0675d7dd8bdabd504b
Francis Rodrigues <dev@tosystems.net>
2017-11-06 14:11:09 -0200

    Implemented Blink component calling setState

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { AppRegistry } from 'react-native';
 // import App from './src/App';
 // import App from './src/HelloWorldApp'
 // import App from './src/Bananas'
-import App from './src/Greeting'
+// import App from './src/Greeting'
+import App from './src/Blink'
 
 AppRegistry.registerComponent('reactNativeApp', () => App);
